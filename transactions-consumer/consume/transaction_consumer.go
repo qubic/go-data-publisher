a0af537346e1ac9c8f16ea9532e8a991fe27ed5c
mio <180114449+qubicmio@users.noreply.github.com>
2026-05-07 12:59:23 +0000

    Treat transactions with positive amount as not ephemeral

--- transactions-consumer/consume/transaction_consumer.go
+++ transactions-consumer/consume/transaction_consumer.go
@@ -117,7 +117,7 @@ func (c *TransactionConsumer) consumeBatch() (int, error) {
 			}
 			document := extern.EsDocument{Id: transaction.Hash, Payload: val}
 
-			if c.isEphemeral(transaction.InputType, transaction.Dest) {
+			if c.isEphemeral(transaction.InputType, transaction.Dest, transaction.Amount) {
 				ephemeralDocuments = append(ephemeralDocuments, document)
 			} else {
 				permanentDocuments = append(permanentDocuments, document)
@@ -158,9 +158,12 @@ func (c *TransactionConsumer) consumeBatch() (int, error) {
 	return len(permanentDocuments) + len(ephemeralDocuments), nil
 }
 
-func (c *TransactionConsumer) isEphemeral(inputType uint32, dest string) bool {
+func (c *TransactionConsumer) isEphemeral(inputType uint32, dest string, amount int64) bool {
 	const zeroAddress = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFXIB"
-	return len(c.ephemeralInputTypes) > 0 && slices.Contains(c.ephemeralInputTypes, inputType) && dest == zeroAddress
+	return len(c.ephemeralInputTypes) > 0 &&
+		slices.Contains(c.ephemeralInputTypes, inputType) &&
+		dest == zeroAddress &&
+		amount == 0
 }
 
 func unmarshalTickTransactions(record *kgo.Record, tickTransactions *TickTransactions) error {
